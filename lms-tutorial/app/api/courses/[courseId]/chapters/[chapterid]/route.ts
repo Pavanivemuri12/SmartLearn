9636fd09332a1bd685f780636daf59fb342ea6c1
Divya7685 <asdivya0503@gmail.com>
2025-05-03 22:49:00 +0530

    Update route.ts

--- lms-tutorial/app/api/courses/[courseId]/chapters/[chapterid]/route.ts
+++ lms-tutorial/app/api/courses/[courseId]/chapters/[chapterid]/route.ts
@@ -10,6 +10,87 @@ const mux = new Mux({
   tokenSecret: process.env.MUX_TOKEN_SECRET!,
 });
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   context: { params: { courseId: string; chapterId: string } }
@@ -18,7 +99,6 @@ export async function PATCH(
     const { userId } = await auth();
     const { isPublished, ...values } = await req.json();
 
-    
     const { courseId, chapterId } = context.params;
 
     if (!userId) {
@@ -43,7 +123,7 @@ export async function PATCH(
       },
       data: {
         ...values,
-        isPublished, // ✅ Include this only if needed, or remove if unused
+        isPublished,
       },
     });
 
