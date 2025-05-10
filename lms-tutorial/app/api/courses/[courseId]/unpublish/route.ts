61e28c92ea6e3a76be840c4c37d5e3f87aa949ad
DevOps507l <asmabegum102004@gmail.com>
2025-05-10 14:28:40 +0530

    Publish & unpublish actions

--- lms-tutorial/app/api/courses/[courseId]/unpublish/route.ts
+++ lms-tutorial/app/api/courses/[courseId]/unpublish/route.ts
@@ -17,46 +17,28 @@ export async function PATCH(
       where: {
         id: params.courseId,
         userId,
-      },
-      include: {
-        chapters: {
-          include: {
-            muxData: true,
-          },
-        },
+      
       },
     });
 
 if (!course) {
   return new NextResponse("Not found", { status: 404 });
 }
-
-const hasPublishedChapter = course.chapters.some((chapter) => chapter.isPublished);
-
-if (
-  !course.title ||
-  !course.description ||
-  !course.imageUrl ||
-  !course.categoryId ||
-  !hasPublishedChapter
-) {
-  return new NextResponse("Missing required fields", { status: 401 });
-}
-    const publishedCourse = await db.course.update({
+    const unpublishedCourse = await db.course.update({
       where: {
         id: params.courseId,
         userId,
       },
       data: {
-        isPublished: true,
+        isPublished: false,
       },
     });
 
-    return NextResponse.json(publishedCourse);
+    return NextResponse.json(unpublishedCourse);
 
 
   } catch (error) {
-    console.log("[COURSE_ID_PUBLISH]", error);
+    console.log("[COURSE_ID_UNPUBLISH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
