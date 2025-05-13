93c056eb3aa3cbc33643eee87a3fcf01f7709751
Pavanivemuri12 <153835795+Pavanivemuri12@users.noreply.github.com>
2025-05-13 21:42:40 +0530

    stripe integration

--- lms-tutorial/middleware.ts
+++ lms-tutorial/middleware.ts
@@ -1,11 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/uploadthing'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/uploadthing','/api/webhook',])
 
 export default clerkMiddleware(async (auth, req) => {
+  
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
+
 })
 
 export const config = {
